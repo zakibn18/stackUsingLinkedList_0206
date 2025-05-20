a76e9744f1fae8815fa4471e7c6563fdddbee341
zakibn18 <jekben815@gmail.com>
2025-05-20 10:31:33 +0700

    membuat case 1

--- stackLinkedList.cpp
+++ stackLinkedList.cpp
@@ -87,7 +87,11 @@ int main()
 
     switch (choice)
     {
-   
+    case 1:
+      cout << "Enter the value to push : ";
+      cin >> value;
+      stact.push(value);
+      break;
     }
   }
   
