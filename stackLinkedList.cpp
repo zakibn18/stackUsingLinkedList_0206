c531398be1ebdbfa1dc06d578e9a456fb267e19f
zakibn18 <jekben815@gmail.com>
2025-05-20 10:05:21 +0700

    membuat prosedur push

--- stackLinkedList.cpp
+++ stackLinkedList.cpp
@@ -23,6 +23,16 @@ public:
   {
     top = NULL;
   }
+
+  int push(int value)
+  {
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    cout << "Push Value : " << value << endl;
+    return value;
+  }
 };
 
 int main()
