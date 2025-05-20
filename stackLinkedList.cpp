49db9ae3a4406971217ad10b642da24c0d203293
zakibn18 <jekben815@gmail.com>
2025-05-20 10:29:01 +0700

    membuat display menu

--- stackLinkedList.cpp
+++ stackLinkedList.cpp
@@ -75,4 +75,15 @@ int main()
 
   int choice = 0;
   int value;
+
+  while (choice != 5)
+  {
+    cout << "1. Push\n";
+    cout << "2. Pop\n";
+    cout << "3. Peek\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice : ";
+    cin >> choice;
+  }
+  
 }
